760d0f8896deae50913d552b150e3befc5e66aef
Mauritshidde <Mauritshidde@gmail.com>
2025-06-04 16:55:07 +0200

    werkende code voor berekenen snelheid in cm/s
    de code voor het berkenen van de snelheid van de bal in cm/s werkend voor als de camera schuin staat.

--- Code/track.py
+++ Code/track.py
@@ -1,8 +1,8 @@
 import math as m
 
-def get_translated_position(d): # Translate the x and y coordinates of the ball into a single direction, give as input the openend file
+def get_data_from_file(d): # Data from file
     x = d.readline()
-    positions = []
+    raw_data = []
     while x:
         data = x.split(",")
         # turn the strings into floats
@@ -10,22 +10,31 @@ def get_translated_position(d): # Translate the x and y coordinates of the ball 
         data[2] = float(data[2])
         data[3] = float(data[3])
         # get the distance using Pythagoras
-        distance = m.sqrt(m.pow(data[2], 2) + m.pow(data[3], 2))
-        positions.append([data[1], distance])
+        raw_data.append([data[1], data[2], data[3]])
         x = d.readline()
-    return positions
+    d.close() # close the data file
+    return raw_data
 
 def get_pixels_per_second(pos): # geef de getransleerde positie in enkele richting mee aan de functie
     velocities = []
     for i in range(len(pos) - 1):
-        delta_x = pos[i][1] + pos[i+1][1]
-        delta_t = pos[i][0] + pos[i+1][0]
-        velocity = delta_x/delta_t
+        delta_y = pos[i+1][2] - pos[i][2]
+        delta_x = pos[i+1][1] - pos[i][1]
+        delta_t = pos[i+1][0] - pos[i][0]
+        velocity = m.sqrt(delta_x**2 + delta_y**2)/delta_t
         velocities.append([[pos[i][0], pos[i+1][0]], velocity])
     return velocities
 
+def get_cm_per_pixel(data): # calulate how many cm a pixel is
+    delta_x = data[0][1] - data[-1][1]
+    delta_y = data[0][2] - data[-1][2]
+    tot_dist_pixels = m.sqrt(delta_x**2 + delta_y**2)
+    print(length/tot_dist_pixels, length, tot_dist_pixels)
+    return tot_dist_pixels
+
+
 # get start values
-folder = "Meeting 1"
+folder = "Meting 1"
 data = open("data/" + folder + "/data.ascii")
 start = data.readline() # remove first non date line
 
@@ -33,10 +42,26 @@ length_file = open("data/" + folder + "/lengte.ascii")
 start = length_file.readline() # remove first non date line
 
 # get the length and close the file
-length = length_file.readline()
+length = float(length_file.readline())
 length_file.close()
 
-pos = get_translated_position(data)
-velocities = get_pixels_per_second(pos)
+data = get_data_from_file(data)
+vel = get_pixels_per_second(data)
+pm = get_cm_per_pixel(data)
 
-data.close()
+print(vel[0][1] * pm, " cm per seconde")
+
+for i in vel:
+    print(i)
+# start calculating
+
+
+
+
+# pos = get_translated_position(data)
+# velocities = get_pixels_per_second(pos)
+
+# pos[]
+#
+# for i in velocities:
+#     print(i)
