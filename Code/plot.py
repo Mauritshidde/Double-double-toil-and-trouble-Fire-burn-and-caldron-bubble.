8de5b06a8994a0ecbbc644913f5cc78f88510010
Mauritshidde <Mauritshidde@gmail.com>
2025-06-11 15:19:52 +0200

    colde voor alleen verschil in  y richting
    camera goed recht hebben tyijdens meting

--- Code/plot.py
+++ Code/plot.py
@@ -8,62 +8,19 @@ folder = 'Meting 3'
 # def distance_time_plot(distance, cmp):
 
 
-# get start values
+# # get start values
 length = get_length(folder)
 data = get_data_from_file(folder)
-vel = get_pixels_per_second(data)
 cmp = get_cm_per_pixel(data, length)
-print(data[0], data[1], data[2], data[-1], data[-2], data[-3])
-print(cmp)
-# vels = noise_supression(vel, 30)
-vels = get_pixels_per_second_suppresed(data, 30)
-distance = get_distance_per_time(data) # suppresed versie werkt niet
+dist, time = get_dist_traveled(data)
+dist_cm = dist_in_cm(dist, cmp)
 
-x = []
-y = []
-for i in data:
-    x.append(i[1])
-    y.append(i[2])
-
-plt.scatter(x, y, s=1)
+plt.scatter(time, dist, s=1)
 plt.xlabel("tijd in seconden")
-plt.ylabel("afstand in bla")
+plt.ylabel("afstand in pixels")
 plt.show()
 
-# variabelen voor tijd afstand plots
-current_dist = 0
-traveled = [0]
-time = [0]
-for i in distance:
-    current_dist += i[1]
-    traveled.append(current_dist * cmp)
-    time.append(i[0])
-
-#afstand tijd plot
-plt.scatter(time, traveled, s=0.7)
-plt.xlabel("tijd in seconden")
-plt.ylabel("afstand in cm")
-plt.show()
-
-traveled_arr = np.array(traveled)
-plt.scatter(time, traveled_arr**2, s=0.7)
-plt.xlabel("tijd in seconden")
-plt.ylabel("gekwadrateerde afstand in cm")
-plt.show()
-
-#plot 3 als test
-dist2 = dist_from_O(data)
-distance2 = traveled_from_O(dist2)
-
-# current_dist = 0
-# traveled = [0]
-# time = [0]
-# for i in distance2:
-#     current_dist += i[1]
-#     traveled.append(current_dist * cmp)
-#     time.append(i[0])
-
-plt.scatter(dist2[0], dist2[1], s=0.7)
+plt.scatter(time, dist_cm, s=1)
 plt.xlabel("tijd in seconden")
 plt.ylabel("afstand in cm")
 plt.show()
--- Code/track.py
+++ Code/track.py
@@ -22,56 +22,6 @@ def get_data_from_file_helper(d): # Data from file
     d.close() # close the data file
     return raw_data
 
-def get_pixels_per_second(pos): # geef de getransleerde positie in enkele richting mee aan de functie
-    velocities = []
-    for i in range(len(pos) - 1):
-        delta_y = pos[i+1][2] - pos[i][2]
-        delta_x = pos[i+1][1] - pos[i][1]
-        delta_t = pos[i+1][0] - pos[i][0]
-        velocity = m.sqrt(delta_x**2 + delta_y**2)/delta_t
-        velocities.append([[pos[i][0], pos[i+1][0]], velocity])
-    return velocities
-
-def get_pixels_per_second_suppresed(pos, steps):
-    velocities = []
-    i = steps
-    for j in range(len(pos) - 1 - 2 * steps):
-        delta_y = abs(pos[i-steps][2] - pos[i+steps][2])
-        delta_x = abs(pos[i-steps][1] - pos[i+steps][1])
-        delta_t = abs(pos[i-steps][0] - pos[i+steps][0])
-        velocity = m.sqrt(delta_x**2 + delta_y**2)/delta_t
-        velocities.append([[pos[i][0], pos[i+1][0]], velocity])
-        i+=1
-    return velocities
-
-def get_distance_per_time_suppresed(pos, steps):
-    velocities = []
-    i = steps
-    for j in range(len(pos) - 2 - 2 * (steps-1)):
-        delta_y = abs(pos[i-steps][2] - pos[i+steps][2])
-        delta_x = abs(pos[i-steps][1] - pos[i+steps][1])
-        delta_t = pos[i-steps][0] + pos[i+steps][0]
-        distance = m.sqrt(delta_x**2 + delta_y**2)
-        velocities.append([delta_t/2, distance])
-        i+=1
-    return velocities
-
-def get_distance_per_time(pos):
-    velocities = []
-    for i in range(len(pos) - 1):
-        delta_y = abs(pos[i+1][2] - pos[i][2])
-        delta_x = abs(pos[i+1][1] - pos[i][1])
-        delta_t = pos[i+1][0] + pos[i][0]
-        distance = m.sqrt(delta_x**2 + delta_y**2)
-        velocities.append([delta_t/2, distance])
-    return velocities
-
-def get_cm_per_pixel(data, length): # calulate how many cm a pixel is
-    delta_x = data[0][1] - data[-1][1]
-    delta_y = data[0][2] - data[-1][2]
-    tot_dist_pixels = m.sqrt(delta_x**2 + delta_y**2)
-    return length/tot_dist_pixels
-
 def get_data_from_file(folder):
     data = open("data/" + folder + "/data.mqa")
     start = data.readline() # remove first non date line
@@ -92,32 +42,26 @@ def get_length(folder):
     length_file.close()
     return length
 
-def noise_supression(vel, steps): # depens on a velocity set that is big
-    suppresed_vel = []
-    i = steps
-    for j in range(len(vel) - steps*2):
-        vel_tot = vel[i][1]
-        for k in range(steps):
-            vel_tot += vel[i-k-1][1]
-            vel_tot += vel[i+k+1][1]
-        suppresed_vel.append([vel[i][0], vel_tot/(steps + 1)])
-        i+=1
-    return suppresed_vel
 
-def dist_from_O(pos):
-    dist = []
-    for i in pos:
-        d = m.sqrt(pow(i[2], 2) + pow(i[2], 2))
-        dist.append([i[0], d])
-    return dist
+def get_cm_per_pixel(data, length): # calulate how many cm a pixel is
+    delta_x = data[0][1] - data[-1][1]
+    delta_y = data[0][2] - data[-1][2]
+    tot_dist_pixels = m.sqrt(delta_x**2 + delta_y**2)
+    return length/tot_dist_pixels
 
-def traveled_from_O(dist):
-    distances = [[0, 0]]
-    distance = 0
-    for i in range(len(dist) - 1):
-        d = dist[i][1] - dist[i+1][1]
-        print(d, distance)
-        distance += d
-        time = (dist[i+1][0] + dist[i][0])/2
-        distances.append([time, distance])
-    return distances
+def get_dist_traveled(data):
+    dist = 0
+    dists = [0]
+    time = [0]
+    for i in range(len(data) - 1):
+        dist += pow(data[i][2] - data[i+1][2], 2)
+        dists.append(dist)
+        time.append(data[i][2])
+    return dists, time
+
+def dist_in_cm(dist, cmp):
+    dist2 = dist.copy()
+    for i in range(len(dist)):
+        dist2[i] = dist[i] * cmp
+
+    return dist2
